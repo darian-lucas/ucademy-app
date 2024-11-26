4ac3810de8740c1cdf0cc764c3294abd8ed7d792
chithan04 <thanindepent04@gmail.com>
2024-11-26 23:54:02 +0700

    clerk and mongoose

---
+++ src/lib/mongoose.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use server";
+
+import mongoose from "mongoose";
+
+
+// singleton connection: chỉ connect 1 lần, nếu chưa connect thì sẽ connect
+let isConnected: boolean = false;
+export const connectToDatabase = async () => {
+  if (!process.env.MONGODB_URL) {
+    throw new Error("MONGODB_URL is not set");
+  }
+  if (isConnected) {
+    console.log("Mongodb is already connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "Ucademy",
+    });
+    isConnected = true;
+    console.log("Using new database connection");
+  } catch (error) {
+    console.log("Error while connecting to database");
+  }
+};

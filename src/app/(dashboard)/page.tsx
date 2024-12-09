4ae7b4a255bbad1f462d4f259654b221fba3a911
chithan04 <thanindepent04@gmail.com>
2024-12-09 23:08:36 +0700

    fix webhook

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,14 +2,14 @@
 import { CourseGrid } from "@/components/common";
 import CourseItem from "@/components/course/CourseItem";
 import Heading from "@/components/typography/Heading";
-import createUser from "@/lib/actions/user.action";
+// import createUser from "@/lib/actions/user.action";
 
 const page = async () => {
-  const user = await createUser({
-    clerkId: "clerk_123",
-    email: "thanindepent04@gmail.com",
-    username: "thanguyen",
-  });
+  // const user = await createUser({
+  //   clerkId: "clerk_123",
+  //   email: "thanindepent04@gmail.com",
+  //   username: "thanguyen",
+  // });
   return (
     <div>
       <Heading>Khám phá</Heading>

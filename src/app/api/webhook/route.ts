5e9cbc47f717919e6750f3912c9b375a6c69d3fa
chithan04 <thanindepent04@gmail.com>
2024-12-09 23:45:16 +0700

    fix webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,24 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import createUser from "@/lib/actions/user.action";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
+
 export async function POST(req: Request) {
-  const svix_id = headers().get("svix-id") ?? "";
-  const svix_timestamp = headers().get("svix-timestamp") ?? "";
-  const svix_signature = headers().get("svix-signature") ?? "";
+  const svix_id = req.headers.get("svix-id") ?? "";
+  const svix_timestamp = req.headers.get("svix-timestamp") ?? "";
+  const svix_signature = req.headers.get("svix-signature") ?? "";
   if (!process.env.WEBHOOK_SECRET) {
-    throw new Error("WEBHOOK_SECRET is not set");
+    throw new Error("WEBHOOK SECRET is not set");
   }
+
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response("Bad Request", { status: 400 });
   }
+
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
   const sivx = new Webhook(process.env.WEBHOOK_SECRET);
 
-  let msg: WebhookEvent;
+  let msg:WebhookEvent;
 
   try {
     msg = sivx.verify(body, {
@@ -27,13 +30,12 @@ export async function POST(req: Request) {
       "svix-signature": svix_signature,
     }) as WebhookEvent;
   } catch (err) {
-    console.log("🚀 ~ POST ~ err:", err);
     return new Response("Bad Request", { status: 400 });
   }
 
   const eventType = msg.type;
-  if (eventType === "user.created") {
-    // create user to database
+  if(eventType === "user.created") {
+    // created user to database
     const { id, username, email_addresses, image_url } = msg.data;
     const user = await createUser({
       username: username!,

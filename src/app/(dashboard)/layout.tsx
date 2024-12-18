b512ee6258c92e54ea5c28aca8b9f83461da5549
chithan04 <thanindepent04@gmail.com>
2024-12-18 11:35:22 +0700

    course details ui

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,7 +2,7 @@ import Sidebar from "@/components/layout/Sidebar";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="wrapper grid grid-cols-[300px,minmax(0,1fr)] h-screen">
+    <div className="wrapper grid lg:grid-cols-[300px,minmax(0,1fr)] h-screen">
       <Sidebar />
       <main className="p-5">{children}</main>
     </div>
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { menuItems } from "@/constants";
 import { TMenuItem } from "@/types";
 import { ActiveLink } from "../common";
@@ -10,7 +10,7 @@ import { IconUsers } from "../icons";
 const Sidebar = () => {
   const { userId } = useAuth();
   return (
-    <div className="p-5 border-r border-r-gray-200 bg-white dark:bg-grayDarker dark:border-opacity-10 flex flex-col">
+    <div className="hidden p-5 border-r border-r-gray-200 dark:border-opacity-10 bg-white dark:bg-grayDarker lg:flex flex-col">
       <a href="/" className="font-bold text-3xl inline-block mb-5">
         <span className="text-primary">U</span>
         cademy

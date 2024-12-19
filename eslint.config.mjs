61fbca9b1138d8f0f94c6c9b4c213199b4ecbf89
chithan04 <thanindepent04@gmail.com>
2024-12-19 22:20:57 +0700

    update lecture p2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,18 @@
-export default tseslint.config({
+// export default tseslint.config({
+//   rules: {
+//     // Note: you must disable the base rule as it can report incorrect errors
+//     "no-unused-vars": "off",
+//     "@typescript-eslint/no-unused-vars": "error"
+//   }
+// });
+import dotenv from 'dotenv';
+dotenv.config();
+
+const isProduction = process.env.ESLINT_NO_DEV_ERRORS === 'true';
+
+export default {
   rules: {
-    // Note: you must disable the base rule as it can report incorrect errors
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error"
-  }
-});
+    "no-unused-vars": isProduction ? "off" : "warn",
+    "@typescript-eslint/no-unused-vars": isProduction ? "off" : "error",
+  },
+};

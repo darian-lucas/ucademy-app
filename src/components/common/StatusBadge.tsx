e4b174c3ddc6f55af44dde2d2c4547bb3c850509
chithan04 <thanindepent04@gmail.com>
2025-01-04 20:47:14 +0700

    order-manage

---
+++ src/components/common/StatusBadge.tsx
@@ -0,0 +1,23 @@
+import { commonClassNames } from "@/constants";
+import { cn } from "@/lib/utils";
+const StatusBadge = ({
+  item,
+  onClick,
+}: {
+  item?: {
+    className?: string;
+    title: string;
+    type?: string;
+  };
+  onClick?: () => void;
+}) => {
+  return (
+    <span
+      className={cn(commonClassNames.status, item?.className)}
+      onClick={onClick}
+    >
+      {item?.title}
+    </span>
+  );
+};
+export default StatusBadge;
